4909456a88551dc7171ffbfc811f980d202ecbf4
Sebastian <sebastianeckhaus@gmail.com>
2022-02-07 08:36:55 -0800

    Add Gifs to Assets
    Added folders with the gifs I've initially made

--- Lab 3/Bullet.java
+++ Lab 3/Bullet.java
@@ -13,6 +13,9 @@ public class Bullet extends Actor
      * Act - do whatever the Bullet wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
+    GifImage bullet = new GifImage("");
+    
     public void act()
     {
         if(direction == "right") setLocation(getX()+4, getY());
