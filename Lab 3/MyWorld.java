72a6526f063cca4ba3786c93652da08f533e189a
kirahebbel <kirahebbel@gmail.com>
2022-02-07 12:57:20 -0500

    Score Board
    Score Board

--- Lab 3/MyWorld.java
+++ Lab 3/MyWorld.java
@@ -8,16 +8,53 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-
+    private int level = 1;
+    //private int health = 10;
+    Counter counter = new Counter("Score: ");
+    Counter levelCounter = new Counter("Level: ");
+    Counter healthCounter = new Counter("Health: ");
+    //level counter vs level??
     /**
      * Constructor for objects of class MyWorld.
      * 
      */
     public MyWorld()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        // Create a new world with 600x400 cells with a 
+        //cell size of 1x1 pixels.
         super(600, 400, 1);
         Player player = new Player();
-        addObject(player, 300, 200);
+        addObject(player, 450, 200);
+        
+        addObject(counter, 535, 30);
+        
+        levelCounter.add(1);
+        addObject(levelCounter, 540, 60);
+        
+        healthCounter.subtract(1);
+        addObject(healthCounter, 540, 90);
+        
+        //addObject(new ScoreBoard(), 300, 365);
+    }
+    
+    public void upLevel()
+    {
+        level++; //seems repetitive??
+        levelCounter.add(1);
+    }
+    
+    public int getLevel()
+    {
+        return level;
+    }
+
+    public void score()
+    {
+        counter.add(1);
+    }
+    
+    public void health()
+    {
+        healthCounter.subtract(1);
     }
 }
