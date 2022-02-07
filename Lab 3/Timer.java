c690ca50920dfaa3e3812e8b80c995e9ff7ecd4f
Garrett <garrett.grossner@outllook.com>
2022-02-07 09:23:34 -0800

    Timer Class

---
+++ Lab 3/Timer.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Timer here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Timer extends Actor
+{
+    private int timer = 18000;
+    
+    public void act() 
+    {
+        timer--;
+        if (timer % 300 == 0)
+            ((MyWorld)getWorld()).spawnZombie(1);
+        if (timer <= 0) {
+            Greenfoot.stop();
+        }
+    }
+}
