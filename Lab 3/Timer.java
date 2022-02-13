a919af9145684594dd24653038bf1f6e4120b6d8
Garrett <garrett.grossner@outlook.com>
2022-02-12 17:04:41 -0800

    Small changes

--- Lab 3/Timer.java
+++ Lab 3/Timer.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Timer extends Actor
+public class Timer extends UI
 {
     private int timer = 18000;
     public void act() 
